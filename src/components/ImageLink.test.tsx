d8dd927da6a671f59e866b3cf15055c6ba893603
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-02-04 19:46:11 -0500

    Add Contact page with new Email and GitHub images

---
+++ src/components/ImageLink.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { ImageLink } from "./ImageLink";
+import type { ImageLinkProps } from "./ImageLink";
+import Email from "/Email.webp";
+
+const data: ImageLinkProps = {
+  alt: "Sample alt text",
+  href: "https://www.google.com",
+  src: Email,
+};
+
+describe("ImageLink", () => {
+  it("renders the passed in alt text", () => {
+    render(<ImageLink {...data} />);
+
+    expect(screen.getByAltText("Sample alt text")).toBeDefined();
+  });
+
+  it("renders the passed in href", () => {
+    render(<ImageLink {...data} />);
+
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "href",
+      "https://www.google.com"
+    );
+  });
+
+  it("renders the correct image src", () => {
+    render(<ImageLink {...data} />);
+    expect(screen.getByRole("img")).toHaveAttribute("src", data.src);
+  });
+});
